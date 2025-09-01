0bb5b2d0ad7ae3c35be9fbf671d05e4e4a79e9a6
bryan <19828900+pataman3@users.noreply.github.com>
2025-08-31 21:28:15 -0400

    Added symlink setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ln -sf ~/dotfiles/home/.bashrc ~/.bashrc
+
+
+mkdir -p ~/.config/hypr
+
+ln -sf ~/dotfiles/home/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+
+
+mkdir -p ~/.config/waybar/scripts
+
+for file in config style.css; do
+  ln -sf ~/dotfiles/home/.config/waybar/$file ~/.config/waybar/$file
+done
+
+for file in power.sh volume.sh weather.sh weather_mode; do
+  ln -sf ~/dotfiles/home/.config/waybar/scripts/$file ~/.config/waybar/scripts/$file
+done
