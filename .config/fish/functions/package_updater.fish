c213eff692ae0b99b27d9f8676ece1eca99d0b55
bryan <19828900+pataman3@users.noreply.github.com>
2025-08-31 20:40:23 -0400

    Wiping the slate clean

--- .config/fish/functions/package_updater.fish
+++
@@ -1,5 +0,0 @@
-function package_updater -d "Updates Arch packages"
-    sudo pacman -Syyu
-    yay -Syyu
-    flatpak update
-end
--- .doom.d/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-
-(package! elfeed)
-(package! elfeed-org)
-(package! elfeed-goodies)
